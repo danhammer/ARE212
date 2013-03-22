216973a61a6d6c6bc6905d6154178502c23c6b7d
Dan Hammer <dan.s.hammer@gmail.com>
2013-03-22 10:12:38 -0700

    edits through section 7

---
+++ section-07/tester.R
@@ -0,0 +1,9 @@
+
+for (i in 1:10) {
+  x <- rnorm(1000)
+  title <- paste("histogram for iteration", i)
+  fname <- paste("hist-", i, ".png", sep = "")
+  png(filename = fname)
+  hist(x, main = title)
+  dev.off()
+}
